ea1e8f6eb6a781be165575d93744fa5baa43a8a0
Suraj Shaw <surajshaw1711@gmail.com>
2019-09-14 16:51:15 +0530

    Temp(Under Construction)

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
+
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
+*/
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
+});
